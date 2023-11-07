15240a86d6ca6e7b2bac8f434a47af35ef4809d6
Jonathan Rubensson <jonathan@eldfluga.se>
2023-11-07 17:11:23 +0100

    Skeleton of the spawning system. Currently working on the array usability in details.

---
+++ Source/PROJ/CombatManager.cpp
@@ -0,0 +1,57 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "CombatManager.h"
+
+
+// Sets default values
+ACombatManager::ACombatManager()
+{
+	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+}
+
+// Called when the game starts or when spawned
+void ACombatManager::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void ACombatManager::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+}
+
+void ACombatManager::AddEnemy(ACharacter* Enemy)
+{
+	Enemies.Emplace(Enemy);
+}
+
+void ACombatManager::RemoveEnemy(ACharacter* Enemy)
+{
+	Enemies.Remove(Enemy);
+}
+
+void ACombatManager::PostEditChangeChainProperty(FPropertyChangedChainEvent& PropertyChangedEvent)
+{
+	//Needs destructors for the spawn points when they are removed from the array.
+	/*
+	if(PropertyChangedEvent.ChangeType == EPropertyChangeType::ArrayAdd &&
+		PropertyChangedEvent.GetPropertyName() == TEXT("SpawnPoints"))
+	{
+		const int Index = PropertyChangedEvent.GetArrayIndex(TEXT("SpawnPoints"));
+		if(SpawnPoints.Num() > Index && SpawnPoints[Index] == nullptr)
+		{
+			SpawnPoints[Index] = GetWorld()->SpawnActor<ASpawnPoint>(SpawnPointBPClass, GetActorLocation(), GetActorRotation());
+			if(SpawnPoints[Index])
+			{
+				SpawnPoints[Index]->SetActorTransform(GetActorTransform());
+			}
+		}
+	}
+	*/
+	Super::PostEditChangeChainProperty(PropertyChangedEvent);
+}
+
---
+++ Source/PROJ/CombatManager.h
@@ -0,0 +1,72 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "SpawnPoint.h"
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "CombatManager.generated.h"
+
+
+class ASpawnPoint;
+
+USTRUCT(BlueprintType, Category = "Enemy Spawn Wave")
+struct FEnemyWave
+{
+	GENERATED_BODY()
+
+	UPROPERTY(EditAnywhere, Category="Enemy Spawn Wave")
+	TSubclassOf<ACharacter> EnemyClass;
+	
+	UPROPERTY(EditAnywhere, Category="Enemy Spawn Wave")
+	TArray<ASpawnPoint*> SpawnPoints;
+
+	UPROPERTY(EditAnywhere, Category="Enemy Spawn Wave")
+	int NumEnemies;
+
+	UPROPERTY(EditAnywhere, Category="Enemy Spawn Wave")
+	int AllowedRemainingEnemiesForNextWave;
+
+	UPROPERTY(EditAnywhere, Category="Enemy Spawn Wave")
+	int TimeToNextWaveAfterEnemiesKilled;
+};
+
+UCLASS()
+class PROJ_API ACombatManager : public AActor
+{
+	GENERATED_BODY()
+
+public:
+	// Sets default values for this actor's properties
+	ACombatManager();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+	void AddEnemy(ACharacter* Enemy);
+
+	void RemoveEnemy(ACharacter* Enemy);
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category="Enemies")
+	int NumActiveEnemies = 0;
+
+	UPROPERTY(EditAnywhere, Category="Spawn")
+	TSubclassOf<ASpawnPoint> SpawnPointBPClass;
+	
+	UPROPERTY(EditAnywhere, Category="Spawn")
+	TArray<ASpawnPoint*> SpawnPoints;
+
+	UPROPERTY(EditAnywhere, Category="Spawn")
+	TArray<FEnemyWave> Waves;
+
+private:
+	UPROPERTY(VisibleAnywhere, Category="Enemies")
+	TArray<ACharacter*> Enemies;
+
+	virtual void PostEditChangeChainProperty(FPropertyChangedChainEvent& PropertyChangedEvent) override;
+};
