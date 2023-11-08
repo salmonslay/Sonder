627cc3ff815e40b7ad3731fd0b7d1c5109eb3b92
Calle <Statencb@gmail.com>
2023-11-08 11:28:32 +0100

    Pressure plate startar inte om

---
+++ Source/PROJ/SwapSpline.cpp
@@ -0,0 +1,27 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "SwapSpline.h"
+
+// Sets default values
+ASwapSpline::ASwapSpline()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+}
+
+// Called when the game starts or when spawned
+void ASwapSpline::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void ASwapSpline::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
