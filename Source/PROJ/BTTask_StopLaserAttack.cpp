2c5c179658b9811173cca7d89a4956b9a5612b30
Frank Martin Cervell <80358278+ASHIYARI@users.noreply.github.com>
2023-11-22 12:08:13 +0100

    Revert "Revert "Merge remote-tracking branch 'origin/HannesGren' into MartinBranch""
    This reverts commit 0d01f66ac79efcafa082cb03046e941cb7386f32.

---
+++ Source/PROJ/BTTask_StopLaserAttack.cpp
@@ -0,0 +1,40 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "BTTask_StopLaserAttack.h"
+
+#include "AIController.h"
+#include "FlyingEnemyCharacter.h"
+#include "BehaviorTree/BlackboardComponent.h"
+
+UBTTask_StopLaserAttack::UBTTask_StopLaserAttack()
+{
+	NodeName = TEXT("StopLaserAttack");
+}
+
+void UBTTask_StopLaserAttack::OnGameplayTaskActivated(UGameplayTask& Task)
+{
+	Super::OnGameplayTaskActivated(Task);
+}
+
+EBTNodeResult::Type UBTTask_StopLaserAttack::ExecuteTask(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory)
+{
+	Super::ExecuteTask(OwnerComp, NodeMemory);
+
+	bNotifyTick = 1;
+
+	if (OwnerComp.GetAIOwner() == nullptr) 	return EBTNodeResult::Failed; 
+
+	OwnerCharacter = Cast<AFlyingEnemyCharacter>(OwnerComp.GetAIOwner()->GetCharacter());
+
+	if (OwnerCharacter == nullptr) return EBTNodeResult::Failed;
+
+	OwnerLocation = OwnerCharacter->GetActorLocation();
+
+	OwnerComp.GetAIOwner()->GetBlackboardComponent()->SetValueAsFloat("StopAttackTime", OwnerCharacter->UnlockRotationAfterAttackDuration);
+
+	OwnerCharacter->bSetFocusToPlayer = true;
+	OwnerCharacter->OnStopAttackEvent(OwnerCharacter->UnlockRotationAfterAttackDuration);
+	OwnerCharacter->Idle();
+	return EBTNodeResult::Succeeded;
+}
