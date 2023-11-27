07c85b078a76d5147a19a9843314a99f66e970e9
Sofia Lindgren <hi@sofia.kiwi>
2023-11-27 16:54:26 +0100

    Add basic friends list

--- Source/PROJ/StaticsHelper.cpp
+++ Source/PROJ/StaticsHelper.cpp
@@ -3,11 +3,39 @@
 
 #include "StaticsHelper.h"
 
-bool StaticsHelper::ActorIsInFront(const AActor* ActorFrom, const FVector& ToLocation)
+#include "BlueprintDataDefinitions.h"
+
+bool UStaticsHelper::ActorIsInFront(const AActor* ActorFrom, const FVector& ToLocation)
 {
 	const FVector DirToTarget = ToLocation - ActorFrom->GetActorLocation();
 
 	const float Dot = FVector::DotProduct(DirToTarget, ActorFrom->GetActorForwardVector());
 
 	return Dot >= 0; // In front of player if Dot Product is > 0, perpendicular when 0 
+}
+
+TArray<FBPFriendInfo> UStaticsHelper::SortFriendsList(const TArray<FBPFriendInfo>& FriendsList)
+{
+	TArray<FBPFriendInfo> SortedFriendsList = FriendsList;
+
+	SortedFriendsList.StableSort([](const FBPFriendInfo& A, const FBPFriendInfo& B)
+	{
+		return A.DisplayName < B.DisplayName;
+	});
+
+	// sort by playing same game
+	SortedFriendsList.StableSort([](const FBPFriendInfo& A, const FBPFriendInfo& B)
+	{
+		return A.bIsPlayingSameGame > B.bIsPlayingSameGame;
+	});
+
+	// sort not offline -> not online (we count stuff like away too, but not as high as online)
+	SortedFriendsList.StableSort([](const FBPFriendInfo& A, const FBPFriendInfo& B)
+	{
+		return A.OnlineState != EBPOnlinePresenceState::Offline && B.OnlineState != EBPOnlinePresenceState::Online;
+	});
+
+	// sort by online
+
+	return SortedFriendsList;
 }
