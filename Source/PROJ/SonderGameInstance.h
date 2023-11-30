b56f6865e189f79375423f6ac9d47ae90e10a8f2
Salmon <hi@sofia.kiwi>
2023-11-30 11:46:02 +0100

    Replace old GameInstance with C++ inherited

---
+++ Source/PROJ/SonderGameInstance.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "AdvancedFriendsGameInstance.h"
+#include "SonderGameInstance.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PROJ_API USonderGameInstance : public UAdvancedFriendsGameInstance
+{
+	GENERATED_BODY()
+	
+};
