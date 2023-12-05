9e015963970417cc5e77de74cee9ff85d63cfade
Isabel <isabel.d.johansson@gmail.com>
2023-12-05 14:07:29 +0100

    Temp commit

--- Source/PROJ/BTTask_DoPulse.cpp
+++ Source/PROJ/BTTask_DoPulse.cpp
@@ -3,3 +3,27 @@
 
 #include "BTTask_DoPulse.h"
 
+#include "AIController.h"
+#include "RobotBaseState.h"
+
+UBTTask_DoPulse::UBTTask_DoPulse()
+{
+}
+
+void UBTTask_DoPulse::OnGameplayTaskActivated(UGameplayTask& Task)
+{
+	Super::OnGameplayTaskActivated(Task);
+}
+
+EBTNodeResult::Type UBTTask_DoPulse::ExecuteTask(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory)
+{
+	Super::ExecuteTask(OwnerComp, NodeMemory);
+
+	if(const auto RobotBaseState = OwnerComp.GetAIOwner()->GetPawn()->FindComponentByClass<URobotBaseState>())
+	{
+		RobotBaseState->Pulse(); 
+		return EBTNodeResult::Succeeded; 
+	}
+
+	return EBTNodeResult::Failed; 
+}
