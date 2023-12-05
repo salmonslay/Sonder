22ce3964f339279e38f9f2bfa9d637a427a95512
Emil Lundström <emillund2002@gmail.com>
2023-12-05 15:55:42 +0100

    Shadow Soul Components fix

--- Source/PROJ/ShadowCharacter.cpp
+++ Source/PROJ/ShadowCharacter.cpp
@@ -14,7 +14,7 @@ AShadowCharacter::AShadowCharacter()
 	// Create the states 
 	DummyState = CreateDefaultSubobject<UDummyPlayerState>(TEXT("DummyStateNew"));
 
-	EnemyBasicAttack = CreateDefaultSubobject<UEnemyBasicAttack>(TEXT("BasicAttack"));
+	EnemyBasicAttack = CreateDefaultSubobject<UEnemyBasicAttack>(TEXT("EnemyBasicAttack"));
 	EnemyBasicAttack->SetupAttachment(RootComponent);
 
 }
